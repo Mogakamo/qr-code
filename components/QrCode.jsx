8d9e44eecc791773b55b98450aaa611f3d9c8715
Mogakamo <mogaka.amo254@gmail.com>
2022-01-31 16:21:10 +0300

    fiished with the component

--- components/QrCode.jsx
+++ components/QrCode.jsx
@@ -4,8 +4,23 @@ import React from 'react'
 function QrCode() {
   return (
     <div className="h-96 w-72 rounded-xl bg-white ">
-      <div className="relative h-56 m-3 rounded-md">
-        <Image src="/image-qr-code.png" objectFit="cover" className='rounded-md' layout="fill" />
+      <div className="relative m-3 h-56 rounded-md">
+        <Image
+          src="/image-qr-code.png"
+          objectFit="cover"
+          className="rounded-md"
+          layout="fill"
+        />
+      </div>
+      <div className="m-5 flex flex-col items-center justify-center text-center">
+        <h1 className="font-bold text-md text-[#1f3251]">
+          Improve your front-end skills by building projects
+        </h1>
+        <p className="text-[#7b879d] mt-2	text-xs p-2">
+          {' '}
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
       </div>
     </div>
   )
