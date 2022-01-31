c76038680f737f1738abbcc2d928f60fcfd53ba1
Mogakamo <mogaka.amo254@gmail.com>
2022-01-31 16:08:24 +0300

    Added the QrCode Image

--- components/QrCode.jsx
+++ components/QrCode.jsx
@@ -1,9 +1,12 @@
+import Image from 'next/image'
 import React from 'react'
 
 function QrCode() {
   return (
-    <div>
-      <h1>QR code</h1>
+    <div className="h-96 w-72 rounded-xl bg-white ">
+      <div className="relative h-56 m-3 rounded-md">
+        <Image src="/image-qr-code.png" objectFit="cover" className='rounded-md' layout="fill" />
+      </div>
     </div>
   )
 }
